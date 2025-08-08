ded27058be324c422f2b5841759cc245453c9926
AyakaYoimia <wency24@mails.tsinghua.edu.cn>
2025-08-08 16:12:56 +0800

    Update CG_coefficient.py
    Solve Problem 2.

--- CG_coefficient.py
+++ CG_coefficient.py
@@ -17,15 +17,13 @@ def CG_coefficients(j_1, m_1, j_2, m_2, J, M):
         if m_1 + m_2 != M or m_1 > j_1 or m_1 < -j_1 or m_2 > j_2 or m_2 < -j_2:
             return 0
         elif M == J:
-            summary = sum([CG_coefficients(j_1, j_1, j_2, M-j_1, j, M) ** 2 for j in range(J, j_1 + j_2 + 1)])
             if m_1 == j_1:
+                summary = sum([CG_coefficients(j_1, j_1, j_2, M-j_1, j/2, M) ** 2 for j in range(int(2*J+2), int(2*(j_1 + j_2 + 1)), 2)])
                 return (1 - summary) ** 0.5
             else:
-                def solution(x):
-                    A = np.array([[CG_coefficients(j_1, j_1-i, j_2, M-j_1+i, j, M) for i in range(1, j_2+1)] for j in range(j_1+j_2, J, -1)])
-                    b = np.array([-CG_coefficients(j_1, j_1, j_2, M-j_1, j, M) * CG_coefficients(j_1, j_1, j_2, M-j_1, J, M) for j in range(j_1+j_2, J, -1)])
-                    sol = linalg.solve(A, b)
-                    return sol[x]
-                return solution(j_1 - m_1 - 1)
+                A = np.array([[CG_coefficients(j_1, j_1-i/2, j_2, M-j_1+i/2, j/2, M) for i in range(2, int(2*(j_1+j_2-J+1)), 2)] for j in range(int(2*(j_1+j_2)), int(2*J), -2)])
+                b = np.array([(-1) * CG_coefficients(j_1, j_1, j_2, M-j_1, j/2, M) * CG_coefficients(j_1, j_1, j_2, M-j_1, J, M) for j in range(int(2*(j_1+j_2)), int(2*J), -2)])
+                solution = linalg.solve(A, b)
+                return solution[int(j_1 - m_1 - 1)]
         else:
             return sqrt((j_1*(j_1+1)-m_1*(m_1+1)) / (J*(J+1)-M*(M+1))) * CG_coefficients(j_1, m_1+1, j_2, m_2, J, M+1) + sqrt((j_2*(j_2+1)-m_2*(m_2+1)) / (J*(J+1)-M*(M+1))) * CG_coefficients(j_1, m_1, j_2, m_2+1, J, M+1)
